303cb194e4aabad96d9ad52792f65abb4326f2f5
José Manuel <expressmillionaire@hotmail.com>
2018-04-14 15:15:52 +0200

    login form component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 
-
 import React, { Component } from 'react'
 import { View, Text } from 'react-native'
-
 import firebase from 'firebase'
-
-
 import { Header } from './components/common'
 
 class App extends Component {
+
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: "AIzaSyC3b1OzElh4sv3CRqi6AK5GRhX9VyaLRcI",
+		    authDomain: "auth-app-34978.firebaseapp.com",
+		    databaseURL: "https://auth-app-34978.firebaseio.com",
+		    projectId: "auth-app-34978",
+		    storageBucket: "auth-app-34978.appspot.com",
+		    messagingSenderId: "213258517732"
+		})
+	}
 
 	render() {
 		return(
