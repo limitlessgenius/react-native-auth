c99c14560e39c044cb4329af589929d980409c41
José Manuel <expressmillionaire@hotmail.com>
2018-04-14 15:59:40 +0200

    controlled text inputs

--- src/components/component_login_form.js
+++ src/components/component_login_form.js
@@ -1,23 +1,44 @@
 
 import React, { Component } from 'react'
-import { View, TextInput } from 'react-native'
+import { TextInput } from 'react-native'
 import { CardSection, Button, Card } from './common'
 
 class LoginForm extends Component {
 
+	constructor(props){
+		super(props)
+		this.state = {
+			email: '', 
+			password: '', 
+		}
+	}
+
+	onEmailChangeHandler(email) {
+		this.setState({ email })
+	}
+
+	onPasswordChangeHandler(password) {
+		this.setState({ password })
+	}
+
 	render() {
+		const { inputStyle } = styles	
 		return(
 			<Card>
 					
 				<CardSection>
 					<TextInput 
-						style={styles.inputStyle}
+						style={inputStyle}
+						onChangeText={(text) => this.onEmailChangeHandler(text)}
+						value={this.state.email}
 					/>
 				</CardSection>
 
 				<CardSection>
 					<TextInput 
-						style={styles.inputStyle}
+						style={inputStyle}
+						onChangeText={(text) => this.onPasswordChangeHandler(text)}
+						value={this.state.password}
 					/>
 				</CardSection>
 
@@ -33,7 +54,7 @@ class LoginForm extends Component {
 const styles = {
 	inputStyle: {
 		alignSelf: 'stretch', 
-		heigth: 20
+		height: 25
 	}
 }
 
